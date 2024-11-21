df8c33c9eeb260defa7f1cfcf1fef73e50174c9b
Artur Pyra <artur@pyra.net.pl>
2024-11-21 08:25:22 +0100

    added CORS setting to proxy and lambda

--- WarhammerDiscordBot/discord_bot/app.py
+++ WarhammerDiscordBot/discord_bot/app.py
@@ -12,6 +12,22 @@ discord_message_types = {'PING':1,
                          'MESSAGE_COMPONENT':3,
                          'MESSAGE':4,
                          'PONG':1}
+def build_response(status_code, body):
+    return {
+        'statusCode': status_code,
+        'body': json.dumps(body),
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, GET'
+        }
+    }
+
+basic_response = """{'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, GET'
+            }"""
 def check_if_test_call(event):
     if 'x-test' in event['headers'] and event['headers']['x-test'] == 'ping':
         log.info("Test call received")
@@ -25,10 +41,7 @@ def lambda_handler(event, context):
         log.debug(f"Headers: {event['headers']} Event: {event}")
 
         if check_if_test_call(event):
-            return {
-                'statusCode': 200,
-                'body': json.dumps('test successful')
-            }
+            return build_response(200, 'test successful')
 
         try:
             public_key = get_secret()
@@ -45,31 +58,20 @@ def lambda_handler(event, context):
 
         except KeyError:
             log.error("Invalid request signature, missing headers")
-            return {
-                'statusCode': 401,
-                'body': json.dumps('DHBError: invalid request signature')
-            }
+            return build_response(401, 'DHBError: invalid request signature')
 
             # handle the interaction
             message_type = body['type']
             log.debug(f"Processing message type: {message_type}")
             if message_type == discord_message_types['PING']: #t == 1:
                 log.info("Received ping, responding with pong")
-                return {
-                    'statusCode': 200,
-                    'body': json.dumps({
-                        'type': discord_message_types['PONG']
-                    })
-                }
+                return build_response(200, "{'type': discord_message_types['PONG']}")
             elif message_type == discord_message_types['APPLICATION_COMMAND']: #t == 2:
                 log.info("Processing command received")
                 return command_handler(body)
             else:
                 log.error(f"Unhandled request type: {message_type}")
-                return {
-                    'statusCode': 400,
-                    'body': json.dumps('DHBError: unhandled request type')
-                }
+                return build_response(400, 'DHBError: unhandled request type')
     except:
         raise
 
