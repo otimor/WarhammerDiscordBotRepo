0657b6f3d92b4ee3b0d2b4642fa04671a686766c
Artur Pyra <artur@pyra.net.pl>
2024-11-23 22:05:31 +0100

    fixing import problem

---
+++ WarhammerDiscordBot/discord_bot/discord_tools.py
@@ -0,0 +1,6 @@
+#https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-interaction-type
+interactions_types = {"PING":1,
+                              "APPLICATION_COMMAND":2,
+                              "MESSAGE_COMPONENT":3,
+                              "APPLICATION_COMMAND_AUTOCOMPLETE":4,
+                              "MODAL_SUBMIT":5}
