82e924b80a35043ffb4fc3595e8d52f3744a61ef
Artur Pyra <artur@pyra.net.pl>
2024-11-23 14:37:04 +0100

    additionall loging enabled

--- WarhammerDiscordBot/hello_world/app.py
+++ WarhammerDiscordBot/hello_world/app.py
@@ -4,10 +4,15 @@ from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 
 PUBLIC_KEY = '8327fc9706afcfc96f72c1c0010b13e6af4f6d1c3b61dfd48d867270501525ca'
+import logging
 
+#log.basicConfig(level=log.DEBUG)
+log = logging.getLogger()
+log.setLevel("DEBUG")
 
 def lambda_handler(event, context):
     #we will convert that
+    log.info(f"Received call to proxyGateway: {event}")
     try:
         body = json.loads(event['body'])
 
@@ -15,6 +20,7 @@ def lambda_handler(event, context):
             signature = event['headers']['x-signature-ed25519']
             timestamp = event['headers']['x-signature-timestamp']
         except KeyError:
+            log.warning("Invalid request signature, missing headers ")
             return {
                 'statusCode': 401,
                 'body': json.dumps('invalid request signature')
@@ -30,6 +36,7 @@ def lambda_handler(event, context):
         try:
             verify_key.verify(message.encode(), signature=bytes.fromhex(signature))
         except BadSignatureError:
+            log.error("Invalid request signature")
             return {
                 'statusCode': 401,
                 'body': json.dumps('invalid request signature')
@@ -40,6 +47,7 @@ def lambda_handler(event, context):
         t = body['type']
 
         if t == 1:
+            log.info("Received PING")
             return {
                 'statusCode': 200,
                 'body': json.dumps({
@@ -59,7 +67,7 @@ def lambda_handler(event, context):
 
 def command_handler(body):
     command = body['data']['name']
-
+    log.info(f"Received command: {command}")
     if command == 'hello':
         return {
             'statusCode': 200,
@@ -72,6 +80,7 @@ def command_handler(body):
             })
         }
     elif command == 'test':
+        log.info("Test command received")
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'application/json'},
@@ -83,6 +92,7 @@ def command_handler(body):
             })
         }
     else:
+        log.warning("Unhandled command received")
         return {
             'statusCode': 400,
             'body': json.dumps('unhandled command')
