e2020efe772952f83e725fcea047a6a20e411832
Artur Pyra <artur@pyra.net.pl>
2024-11-26 09:08:34 +0100

    refactor commands to simplify the code

--- WarhammerDiscordBot/discord_bot/warhammer_bot.py
+++ WarhammerDiscordBot/discord_bot/warhammer_bot.py
@@ -1,19 +1,14 @@
 import json
+import logging
+
 
 def hello():
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps({
-            'type': 4,
-            'data': {
-                'content': 'Hello, World from CodeCatalyst AWS hello',
-            }
-        })
-    }
+    return 'Hello, World from CodeCatalyst AWS hello',
+
 
 
 def test():
+    logging.debug("Generating test result command)
     return {
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
